2ebea9e52e5d99a669dba43399de5d63297f8f86
Fredy Alejandro Pinto Pinto <fpintopi@everis.com>
2019-06-21 11:01:06 -0500

    subida de codigo

---
+++ Operaciones/src/main/java/OperacionesMatematicas.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fpintopi
+ */
+public class OperacionesMatematicas {
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+}
---
+++ Operaciones/src/test/java/TestMaven.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author fpintopi
+ */
+public class TestMaven {
+
+    public TestMaven() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void pruebaSuma() {
+        int a = 2, b = 2;
+
+        OperacionesMatematicas op = new OperacionesMatematicas();
+        Assert.assertEquals(op.suma(a, b), 4);
+
+    }
+}
